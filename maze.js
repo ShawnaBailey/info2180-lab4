a8ec43f42ed6aec598c885037af19a75f1fccbb1
ShawnaBailey <44042651+ShawnaBailey@users.noreply.github.com>
2018-10-22 14:57:37 -0500

    first commit

---
+++ maze.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+
+      let leftside = document.getElementById("boundary1");
+       leftside.addEventListener("mouseover", function(){
+         this.setAttribute("class", "boundary youlose");
+      });
+
+  
+}
